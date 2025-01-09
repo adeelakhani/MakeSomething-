de7c7b54cc7d2d2a1dd575ae862ed293d82b7c5e
Adeel Akhani <akhaniadeel@gmail.com>
2025-01-08 22:18:02 -0500

    form data

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,16 +1,42 @@
+import React, {useState} from "react";
 
 export default function Body()
 {
+    //const ingredients = ["cheese", "bread", "ice cream", "tomato", "lettuce"]; 
+    const [ingredients, setIngredients] = useState([]);
+    const ingredientsLists = ingredients.map((ingredient) =>(
+        <li key = {ingredient}>{ingredient}</li>
+    ))
+    // function handleSubmitting(e) 
+    // {
+    //     e.preventDefault();
+    //     const formEl = e.currentTarget;
+    //     const formData = new FormData(formEl);
+    //     const newIngredient = formData.get("ingredient");
+    //     setIngredients(prev => [...prev, newIngredient]);
+    //     formEl.reset();
+    // };
+    //<form onSubmit={handleSubmitting} className="body">
+    function handleSubmitting(formData) 
+    {
+        const ingredient = formData.get("ingredient");
+        setIngredients(prev => [...prev, ingredient]);
+
+    }
     return(
-        <main className="body">
-            <form>
+        <main>
+            <form action={handleSubmitting} className="body">
                 <input 
                 type="input" 
+                name="ingredient"
                 aria-label="add an ingredient" 
                 placeholder="e.g. cheese"
                 />
                 <button>+ Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientsLists}
+            </ul>
 
         </main>
 )
