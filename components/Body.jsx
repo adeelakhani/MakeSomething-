b77afada9275de482fc96a6f40cd3da4e3cd58d9
Adeel Akhani <akhaniadeel@gmail.com>
2025-01-09 23:40:17 -0500

    checkpoint changing code

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,54 +1,44 @@
-import {useState} from "react";
+import { useState } from "react";
 
-export default function Body()
-{
-    const [ingredients, setIngredients] = useState([]);
-    const ingredientsLists = ingredients.map((ingredient) =>(
-        <li key = {ingredient}>{ingredient}</li>
-    ))
-    let numIngredients = ingredients.length;
-    // function handleSubmitting(e) 
-    // {
-    //     e.preventDefault();
-    //     const formEl = e.currentTarget;
-    //     const formData = new FormData(formEl);
-    //     const newIngredient = formData.get("ingredient");
-    //     setIngredients(prev => [...prev, newIngredient]);
-    //     formEl.reset();
-    // };
-    //<form onSubmit={handleSubmitting} className="body">
-    function handleSubmitting(formData) 
-    {
-        const getIngredient = Object.fromEntries(formData);
-        //  const newIngredient = formData.get("ingredient");
-        setIngredients(prev => [...prev, getIngredient.ingredient]);
-
-    }
-    return(
-        <main>
-            <form action={handleSubmitting} className="body">
-                <input 
-                type="input" 
-                name="ingredient"
-                aria-label="add an ingredient" 
-                placeholder="e.g. cheese"
-                />
-                <button>+ Add Ingredient</button>
-            </form>
-            <section>
-            <h2 className = "ingredientsHeader">Ingredients(so far)</h2>
-            <ul className="ingredientsList">
-                {ingredientsLists}
-            </ul>
-            {numIngredients > 3 && 
-            <div className="readyBox">
-                <h1>Ready to submit?</h1>
-                <button>See recipe</button>
-            </div>}
-            
-            </section>
-            
-
-        </main>
-)
+export default function Body() {
+  const [ingredients, setIngredients] = useState([]);
+  const [isShown, setShown] = useState(false);
+  const ingredientsLists = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+  let numIngredients = ingredients.length;
+  function handleSubmitting(formData) {
+    const getIngredient = Object.fromEntries(formData);
+    setIngredients((prev) => [...prev, getIngredient.ingredient]);
+  }
+  function showRecipe(){
+    setShown(true)
+  }
+  return (
+    <main>
+      <form action={handleSubmitting} className="body">
+        <input
+          type="input"
+          name="ingredient"
+          aria-label="add an ingredient"
+          placeholder="e.g. cheese"
+        />
+        <button>+ Add Ingredient</button>
+      </form>
+      <section>
+        <h2 className="ingredientsHeader">Ingredients(so far)</h2>
+        <ul className="ingredientsList">{ingredientsLists}</ul>
+        {numIngredients > 3 && (
+          <div className="readyBox">
+            <h1>Ready to submit?</h1>
+            <button onClick={showRecipe}>See recipe</button>
+          </div>
+        )}
+      </section>
+      {isShown && <section>
+        
+      </section>}
+      
+    </main>
+  );
 }
